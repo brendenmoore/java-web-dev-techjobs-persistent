7a5c442cb6521ae6a019485de149f99a90671d77
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-08-28 15:47:39 -0400

    set up abstract entity

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,12 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name must not be blank")
+    @Length(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
     private String name;
 
     public int getId() {
