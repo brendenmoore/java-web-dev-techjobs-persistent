ca7ae1cfce3b8e4a39cb9ea642d578fb06ad87af
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-08-28 15:50:41 -0400

    connect employer model

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Length(min = 3, max = 100, message = "Length must be between 3 and 100 characters")
+    private String location;
 
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
