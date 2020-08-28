db523229109b9c44b8e9afa3cfe892b5a505eb8f
Brenden <52762708+brendenmoore@users.noreply.github.com>
2020-08-28 17:30:05 -0400

    final sql question answered

--- queries.sql
+++ queries.sql
@@ -8,3 +8,8 @@ SELECT name FROM techjobs.employer WHERE location = "St. Louis City";
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT DISTINCT name, description
+FROM skill
+INNER JOIN job_skills
+ON skill.id = job_skills.skills_id
+ORDER BY name;
